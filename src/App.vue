6bbc0200ee2d7c90aa436d3df8dd06658099b4a0
Walter Garcia <waegarcia@gmail.com>
2022-03-21 06:46:05 -0300

    update main con desafio formulario

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="grey lighten-5"
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Coderhouse Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.jpg"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>
-      <h1>Login:</h1>
-      <Login/>
-      <v-divider></v-divider><br>
-      <h1>Registro:</h1>
-      <Registro/>
-      <v-divider></v-divider><br>
-      <h1>Menu de comidas segun pais:</h1>
-      <Listado/>
-      <v-divider></v-divider><br>
-    </v-main>
-  </v-app>
+<div>
+  <Formulario/>
+</div>
 </template>
 
 <script>
-import Login from './components/Login';
-import Registro from './components/Registro';
-import Listado from './components/Listado'
+import Formulario from './components/Formulario.vue'
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    Login,
-    Registro,
-    Listado
+    Formulario,
   },
-
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
+
+<style scoped>
+</style>
---
+++ src/components/Tabla.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-card class="col-9 mt-3">
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Nombre</th>
+              <th class="text-left">email</th>
+              <th class="text-left">Pais</th>
+              <th class="text-left">Consentimiento</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, i) in tablaDatos" :key="i">
+              <td>{{ item.name }}</td>
+              <td>{{ item.email }}</td>
+              <td>{{ item.pais }}</td>
+              <td>{{ item.check }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Tabla",
+  props: {
+    datos: {
+      type: Object,
+      required: true
+    },
+  },
+  data() {
+    return {
+      listaDatos: []
+    };
+  },
+  methods: {
+    actualizarRegistros(){
+      if (Object.keys(this.datos).length !== 0) {
+        this.listaDatos.push(this.datos);
+      }
+    }
+  },
+  computed: {
+    tablaDatos() {
+      this.actualizarRegistros();
+      console.log(this.listaDatos);
+      return this.listaDatos;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
